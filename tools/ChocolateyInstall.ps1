9e9ab99a44dc72326fb02d6b3f50254ddbde3ada
ruusty <russell@holliday.id.au>
2019-04-04 23:48:34 +1100

    Changed Chocolatey install path to $env:LOCALAPPDATA\..\..\.PowershellModules

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -2,8 +2,8 @@ write-host chocolateyPackageFolder  =$env:chocolateyPackageFolder
 write-host chocolateyPackageName    =$env:chocolateyPackageName
 write-host chocolateyPackageVersion =$env:chocolateyPackageVersion
 
-write-host `$ErrorActionPreference=$ErrorActionPreference
-write-host `$VerbosePreference=$VerbosePreference
+#write-host `$ErrorActionPreference=$ErrorActionPreference
+#write-host `$VerbosePreference=$VerbosePreference
 
 $tools = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 .  $(join-path $tools "properties.ps1")
@@ -21,13 +21,10 @@ $psModulePath = [Environment]::GetEnvironmentVariable('PSModulePath','Machine')
 # if installation dir path is not already in path then add it.
 if(!($psModulePath.Split(';').Contains($installRootDirPath))){
   Write-Debug "adding $installRootDirPath to '$env:PSModulePath'"
-
     # trim trailing semicolon if exists
     $psModulePath = $psModulePath.TrimEnd(';');
-
     # append path
     $psModulePath = $psModulePath + ";$installRootDirPath"
-
     # save
     Install-ChocolateyEnvironmentVariable -variableName "PSModulePath" -variableValue $psModulePath -variableType 'Machine'
     # make effective in current session
--- tools/ChocolateyUninstall.ps1
+++ tools/ChocolateyUninstall.ps1
@@ -2,13 +2,11 @@ write-host chocolateyPackageFolder  =$env:chocolateyPackageFolder
 write-host chocolateyPackageName    =$env:chocolateyPackageName
 write-host chocolateyPackageVersion =$env:chocolateyPackageVersion
 
-write-host `$ErrorActionPreference=$ErrorActionPreference
-write-host `$VerbosePreference=$VerbosePreference
+#write-host `$ErrorActionPreference=$ErrorActionPreference
+#write-host `$VerbosePreference=$VerbosePreference
 
 $tools = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 .  $(join-path $tools "properties.ps1")
 
 UnInstall-ChocolateyZipPackage -PackageName $env:chocolateyPackageName -ZipFileName $ZipName
-write-host `$lastexitcode=$lastexitcode -ForegroundColor DarkYellow
-write-host `$?=$? -ForegroundColor DarkYellow
-write-host $error
+
